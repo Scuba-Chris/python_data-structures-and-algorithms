1005f41235a49b266435ee4cc5ad2db7ce29c148
Christopher Ceder <cceder@Christophers-MacBook-Air.local>
2020-01-05 21:07:10 -0800

    added to stacks and queues

--- data_structures/stacks_and_queues/test_stacks_and_queues.py
+++ data_structures/stacks_and_queues/test_stacks_and_queues.py
@@ -1,4 +1,4 @@
-from stacks_and_queues import Node, Stack, Queue
+from data_structures.stacks_and_queues.stacks_and_queues import Node, Stack, Queue
 import pytest
 
 def test_push(values):
@@ -18,5 +18,15 @@ def test_peek():
         st.push(num)
 
     expected = 4
+    actual = st.peek()
+    assert expected == actual
+
+def test_pop():
+    st = Stack()
+    values = [1,2,3,4]
+    for _ in values:
+        st.pop()
+
+    expected = None
     actual = st.peek()
     assert expected == actual
