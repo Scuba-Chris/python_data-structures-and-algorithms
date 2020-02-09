88e89993591dabbe25416a17687b56e7bc2ffbe2
Christopher Ceder <cceder@Christophers-MacBook-Air.local>
2020-02-09 14:57:42 -0800

    fixed some spelling errors

--- challenges/depth_first/depth_first.py
+++ challenges/depth_first/depth_first.py
@@ -4,11 +4,11 @@ def depth_first(self, vertex):
     if vertex not in self.get_node:
         raise ValueError
     visited,output = set(),[]
-    def recurese(vertex):
+    def recurse(vertex):
         visited.add(vertex)
         output.append(vertex.value)
         for vert in self.get_node(vertex):
             if vert not in visited:
-                recurese(vert)
-    recurese(vertex)
+                recurse(vert)
+    recurse(vertex)
     return output
